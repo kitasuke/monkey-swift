96ee4e8d3b739db45a468a8e5d0aed87e89fe9f0
kitasuke <yusuke2759@gmail.com>
2018-11-18 15:14:41 +0800

    Move constants to different file

---
+++ Sources/Token/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  Constants
+//
+//  Created by Yusuke Kita on 11/15/18.
+//
+
+public enum TokenSymbol: Character {
+    case equal = "="
+    case plus = "+"
+    case minus = "-"
+    case bang = "!"
+    case asterisk = "*"
+    case slash = "/"
+    case comma = ","
+    case semicolon = ";"
+    case leftParen = "("
+    case rightParen = ")"
+    case leftBrace = "{"
+    case rightBrace = "}"
+    case lessThan = "<"
+    case greaterThan = ">"
+}
+
+public enum TokenKeyword: String {
+    case fn = "fn"
+    case `let` = "let"
+    case `true` = "true"
+    case `false` = "false"
+    case `if` = "if"
+    case `else` = "else"
+    case `return` = "return"
+}
--- Sources/Token/Token.swift
+++ Sources/Token/Token.swift
@@ -5,33 +5,6 @@
 //  Created by Yusuke Kita on 11/15/18.
 //
 
-public enum TokenSymbol: Character {
-    case equal = "="
-    case plus = "+"
-    case minus = "-"
-    case bang = "!"
-    case asterisk = "*"
-    case slash = "/"
-    case comma = ","
-    case semicolon = ";"
-    case leftParen = "("
-    case rightParen = ")"
-    case leftBrace = "{"
-    case rightBrace = "}"
-    case lessThan = "<"
-    case greaterThan = ">"
-}
-
-public enum TokenKeyword: String {
-    case fn = "fn"
-    case `let` = "let"
-    case `true` = "true"
-    case `false` = "false"
-    case `if` = "if"
-    case `else` = "else"
-    case `return` = "return"
-}
-
 public enum TokenType: Equatable {
     case unknown, illegal, eof
 
