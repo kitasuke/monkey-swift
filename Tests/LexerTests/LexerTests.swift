d2b7dec251bb9ce84055e7cd41658119c15899ac
kitasuke <yusuke2759@gmail.com>
2018-12-19 18:21:15 +0900

    Change order of test code

--- Tests/LexerTests/LexerTests.swift
+++ Tests/LexerTests/LexerTests.swift
@@ -14,7 +14,6 @@ final class LexerTests: XCTestCase {
         let input = """
             let five = 5;
             let ten = 10;
-            let two = "two";
 
             let add = fn(x, y) {
                 x + y;
@@ -23,17 +22,19 @@ final class LexerTests: XCTestCase {
             let result = add(five, ten);
             !-/*5;
             5 < 10 > 5;
+
+            let two = "two";
             "foobar"
         """
         
         let expectedTokens: [Token] = [
             .init(type: .let), .makeIdentifier(identifier: "five"), .init(type: .assign), .makeNumber(number: "5"), .init(type: .semicolon),
             .init(type: .let), .makeIdentifier(identifier: "ten"), .init(type: .assign), .makeNumber(number: "10"), .init(type: .semicolon),
-            .init(type: .let), .makeIdentifier(identifier: "two"), .init(type: .assign), .makeString(string: "two"), .init(type: .semicolon),
             .init(type: .let), .makeIdentifier(identifier: "add"), .init(type: .assign), .init(type: .function), .init(type: .leftParen), .makeIdentifier(identifier: "x"), .init(type: .comma), .makeIdentifier(identifier: "y"), .init(type: .rightParen), .init(type: .leftBrace), .makeIdentifier(identifier: "x"), .init(type: .plus), .makeIdentifier(identifier: "y"), .init(type: .semicolon), .init(type: .rightBrace), .init(type: .semicolon),
             .init(type: .let), .makeIdentifier(identifier: "result"), .init(type: .assign), .makeIdentifier(identifier: "add"), .init(type: .leftParen), .makeIdentifier(identifier: "five"), .init(type: .comma), .makeIdentifier(identifier: "ten"), .init(type: .rightParen), .init(type: .semicolon),
             .init(type: .bang), .init(type: .minus), .init(type: .slash), .init(type: .asterisk), .makeNumber(number: "5"), .init(type: .semicolon),
             .makeNumber(number: "5"), .init(type: .lessThan), .makeNumber(number: "10"), .init(type: .greaterThan), .makeNumber(number: "5"), .init(type: .semicolon),
+            .init(type: .let), .makeIdentifier(identifier: "two"), .init(type: .assign), .makeString(string: "two"), .init(type: .semicolon),
             .makeString(string: "foobar")
         ]
 
